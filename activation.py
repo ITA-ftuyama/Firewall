2bd67a915485cc8fe09ba779c20fc5ff87c3eb58
ftuyama <ftuyama@gmail.com>
2016-11-13 15:26:10 -0200

    Activation - Mininet

---
+++ activation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+u"""Ativação customizada do Mininet."""
+
+from mininet.net import Mininet
+from mininet.topolib import TreeTopo
+
+
+tree4 = TreeTopo(depth=2, fanout=2)
+net = Mininet(topo=tree4)
+net.start()
+h1, h4 = net.hosts[0], net.hosts[3]
+print h1.cmd('ping -c1 \%s' % h4.IP())
+net.stop()
