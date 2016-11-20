15de6c4d3530d0806560ce60e92457e0cc7a380c
ftuyama <ftuyama@gmail.com>
2016-11-20 15:36:09 -0200

    UTF

--- switch.py
+++ switch.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
 """Copyright (C) 2011 Nippon Telegraph and Telephone Corporation."""
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
